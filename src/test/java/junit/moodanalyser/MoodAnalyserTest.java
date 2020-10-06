2b5cc3860da1b1b65b4486fed869b6c689bb3093
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-06 16:43:49 +0530

    [Mukesh] update : "I am in Any Mood" checked

--- src/test/java/junit/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/junit/moodanalyser/MoodAnalyserTest.java
@@ -15,7 +15,7 @@ public class MoodAnalyserTest {
 	@Test
 	public void whenGiven_Proper_ShouldReturn_Sad() {
 		MoodAnalyser mood = new MoodAnalyser();
-		String result = mood.analyseMood("This is a happy message");
+		String result = mood.analyseMood("I am in Any Mood");
 		Assert.assertEquals("HAPPY", result);
 	}
 }
